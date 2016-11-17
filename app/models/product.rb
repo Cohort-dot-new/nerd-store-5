ae95e63c8ee75a6b6905440ce10e946b3c760dc2
Peter Jang <peter.x.jang@gmail.com>
2016-11-16 19:21:15 -0600

    Refactor model methods to separate view concerns

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,4 @@
 class Product < ActiveRecord::Base
-  def sale_message
-    if price.to_f < 10
-      "Discount Item!"
-    else
-      "Everyday Value!"
-    end
-  end
-
   def tax
     price.to_f * 0.09
   end
@@ -15,11 +7,7 @@ class Product < ActiveRecord::Base
     price.to_f + tax
   end
 
-  def discount_class_name
-    if sale_message == "Discount Item!"
-      return "discount-item"
-    else
-      return ""
-    end
+  def discounted?
+    price.to_f < 10
   end
 end
