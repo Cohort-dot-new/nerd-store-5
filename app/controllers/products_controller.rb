96813d503a879117ad7b7ed62a75a5711323d1a9
Peter Jang <peter.x.jang@gmail.com>
2016-11-16 18:21:07 -0600

    Use explicit render and save methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,40 +1,42 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    render 'index.html.erb'
   end
 
   def new
+    render 'new.html.erb'
   end
 
   def create
-    @product = Product.create(
+    @product = Product.new(
       name: params[:name],
       description: params[:description],
       image: params[:image],
       price: params[:price]
-      )
-
+    )
+    @product.save
     flash[:success] = "Product Created"
     redirect_to "/products/#{@product.id}"
   end
 
   def show
     @product = Product.find_by(id: params[:id])
+    render 'show.html.erb'
   end
 
   def edit
     @product = Product.find_by(id: params[:id])
+    render 'edit.html.erb'
   end
 
   def update
     @product = Product.find_by(id: params[:id])
-    @product.update(
-      name: params[:name],
-      description: params[:description],
-      image: params[:image],
-      price: params[:price]
-      )
-
+    @product.name = params[:name]
+    @product.description = params[:description]
+    @product.image = params[:image]
+    @product.price = params[:price]
+    @product.save
     flash[:success] = "Product Updated"
     redirect_to "/products/#{@product.id}"
   end
@@ -42,9 +44,8 @@ class ProductsController < ApplicationController
   def destroy
     @product = Product.find_by(id: params[:id])
     @product.destroy
-
-    flash[:warning] = "Product Created"
-    redirect_to "/"
+    flash[:warning] = "Product Destroyed"
+    redirect_to "/products"
   end
 end
 
