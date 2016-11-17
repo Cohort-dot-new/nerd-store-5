4dfd1201224fdc0dda747608e7ecb9b8740f3800
Peter Jang <peter.x.jang@gmail.com>
2016-11-16 18:54:04 -0600

    Add model methods for sale message, tax, and total

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ActiveRecord::Base
+  def sale_message
+    if price.to_f < 10
+      "Discount Item!"
+    else
+      "Everyday Value!"
+    end
+  end
+
+  def tax
+    price.to_f * 0.09
+  end
+
+  def total
+    price.to_f + tax
+  end
 end
