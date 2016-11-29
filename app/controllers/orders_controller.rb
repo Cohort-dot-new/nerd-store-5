58baa79afdbb57fbf014564013e29e0af0d5656f
Peter Jang <peter.x.jang@gmail.com>
2016-11-28 18:28:10 -0600

    Fix order show page to show some info about the order (incomplete)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,10 +3,11 @@ class OrdersController < ApplicationController
     order = Order.new(user_id: current_user.id, quantity: params[:quantity])
     order.save
     flash[:success] = "Your order was created successfully!"
-    redirect_to "/orders/:id"
+    redirect_to "/orders/#{order.id}"
   end
 
   def show
+    @order = Order.find_by(id: params[:id])
     render 'show.html.erb'
   end
 end
