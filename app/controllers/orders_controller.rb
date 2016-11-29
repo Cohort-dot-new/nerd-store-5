15c0bccec67ad81a9bd7e92c8ad0107d652a612e
Peter Jang <peter.x.jang@gmail.com>
2016-11-28 18:34:12 -0600

    Save product id when creating order (in a very user unfriendly way)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   def create
-    order = Order.new(user_id: current_user.id, quantity: params[:quantity])
+    order = Order.new(
+      user_id: current_user.id,
+      quantity: params[:quantity],
+      product_id: params[:product_id]
+    )
     order.save
     flash[:success] = "Your order was created successfully!"
     redirect_to "/orders/#{order.id}"
