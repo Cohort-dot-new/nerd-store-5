2cdc0074959d76db91e9189c56af7f3728b3685d
Peter Jang <peter.x.jang@gmail.com>
2016-11-28 18:21:16 -0600

    Create new order in database (incomplete)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
   def create
+    order = Order.new(user_id: current_user.id, quantity: params[:quantity])
+    order.save
     flash[:success] = "Your order was created successfully!"
     redirect_to "/orders/:id"
   end
