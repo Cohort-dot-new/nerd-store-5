6038913e27969d36a7a75af6cf09d70e850ab0f1
Peter Jang <peter.x.jang@gmail.com>
2016-11-28 19:53:06 -0600

    Add category model with join table and belongs_to / has_many associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   belongs_to :supplier # hey Rails, please write me a method called supplier
   has_many :images # hey Rails, please write me a method called images
   has_many :orders
+  has_many :category_products
 
   def tax
     price.to_f * 0.09
