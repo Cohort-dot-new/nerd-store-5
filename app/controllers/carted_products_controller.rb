c42a8df7051502c43eb382914c3c37d6a0ade7a5
Peter Jang <peter.x.jang@gmail.com>
2016-12-01 19:32:24 -0600

    Redirect users if no items in cart

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,7 +1,12 @@
 class CartedProductsController < ApplicationController
   def index
     @carted_products = current_user.carted_products.where(status: "carted")
-    render 'index.html.erb'
+    if @carted_products.length > 0
+      render 'index.html.erb'
+    else
+      flash[:warning] = "You don't have any items in your shopping cart. BUY SOME STUFF NOW!"
+      redirect_to "/"
+    end
   end
 
   def create
