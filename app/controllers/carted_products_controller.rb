e85c594a6154d4fb4d8424a8f4efbad5b476c8f0
Peter Jang <peter.x.jang@gmail.com>
2016-12-01 18:38:38 -0600

    Change form to add product with quantity to shopping cart

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,13 @@
 class CartedProductsController < ApplicationController
+  def create
+    carted_product = CartedProduct.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      status: "carted"
+    )
+    carted_product.save
+    flash[:success] = "Added to cart successfullly!"
+    redirect_to "/"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   post '/orders' => 'orders#create'
   get '/orders/:id' => 'orders#show'
 
+  post '/carted_products' => 'carted_products#create'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   get '/login' => 'sessions#new'
